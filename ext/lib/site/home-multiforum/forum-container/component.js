877edcda72cc591b0d6d33cec85ec49954d330f4
Inve <inveniahadouken@gmail.com>
2019-09-19 22:56:52 -0300

    refactor

--- ext/lib/site/home-multiforum/forum-container/component.js
+++ ext/lib/site/home-multiforum/forum-container/component.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 import CardsSlider from 'ext/lib/site/cards-slider/component'
 import ForumCard from '../forum-card/component'
 
+const dicContentType = {
+  'ejes': 'Los ejes ',
+  'propuestas': 'Las propuestas '
+}
+
 export default ({ forum }) => (
   <div className='container forum-card-container'>
     <ForumCard forum={forum} />
     <div className='forum-slider-wrapper'>
-      <h4 className='forum-slider-title'>{
+    <h4 className='forum-slider-title'>
+      {/* <h4 className='forum-slider-title'>{
         forum.extra.contentType === 'ejes' && 'Los ejes '
       }
       {
@@ -14,7 +20,9 @@ export default ({ forum }) => (
       }
       {
         forum.extra.contentType === 'propuestas' && 'Las propuestas '        
-      }que comprenden esta consulta son:</h4>
+      } */}
+      {dicContentType[forum.extra.contentType] || 'Los ejes '}
+        que comprenden esta consulta son:</h4>
       <CardsSlider forum={forum} />
     </div>
   </div>
