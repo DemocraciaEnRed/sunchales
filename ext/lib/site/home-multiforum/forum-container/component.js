987758a24ac0041866f32f2442ffcc20c579d18b
Inve <inveniahadouken@gmail.com>
2019-10-02 14:43:45 -0300

    moved button at final forum-card

--- ext/lib/site/home-multiforum/forum-container/component.js
+++ ext/lib/site/home-multiforum/forum-container/component.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { Link }  from 'react-router'
 import CardsSlider from 'ext/lib/site/cards-slider/component'
 import ForumCard from '../forum-card/component'
+import urlBuilder from 'lib/url-builder'
+
 
 const dicContentType = {
   'ejes': 'Los ejes ',
@@ -24,6 +27,13 @@ export default ({ forum }) => (
       {dicContentType[forum.extra.contentType] || 'Los ejes '}
         que comprenden esta consulta son:</h4>
       <CardsSlider forum={forum} />
+      <div className='btn-mobile'>
+        <Link to={urlBuilder.for('site.forum', { forum: forum.name })}>
+          <button className='btn btn-link' >
+            Ver más información
+          </button>
+        </Link>
+        </div>
     </div>
   </div>
 )
